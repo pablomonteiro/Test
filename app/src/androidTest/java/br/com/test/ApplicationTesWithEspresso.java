884341099ebca3956869c276dfe25f33910a62c9
PabloMonteiro <pablomf84@gmail.com>
2016-03-13 14:46:50 -0300

    Alterando configuracao do teste para desbloquear a tela antes da execucao

--- app/src/androidTest/java/br/com/test/ApplicationTesWithEspresso.java
+++ app/src/androidTest/java/br/com/test/ApplicationTesWithEspresso.java
@@ -6,33 +6,70 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.assertion.ViewAssertions.*;
 import static android.support.test.espresso.matcher.ViewMatchers.*;
+
+import android.app.Activity;
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.support.test.annotation.UiThreadTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.util.Log;
+import android.view.WindowManager;
 
 import com.robotium.solo.Solo;
 
 import org.hamcrest.core.AllOf;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Created by pablo on 26/08/15.
  */
 @LargeTest
-public class ApplicationTesWithEspresso extends ActivityInstrumentationTestCase2 {
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTesWithEspresso {
 
-    public ApplicationTesWithEspresso() {
-        super(MainActivity.class);
+    @Rule
+    public final ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    @Before
+//    @UiThreadTest
+    public void before() {
+        final Activity activity = mActivityRule.getActivity();
+        try {
+            mActivityRule.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+//                    KeyguardManager mKG = (KeyguardManager) activity.getSystemService(Context.KEYGUARD_SERVICE);
+//                    KeyguardManager mLock = mKG.newKeyguardLock(KEYGUARD_SERVICE);
+//                    mLock.disableKeyguard();
+
+                    //turn the screen on
+                    activity.getWindow().addFlags(
+                            /*WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                            | */WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD/*
+                            | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                            | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                            | WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON*/);
+                }
+            });
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
     }
 
-    @Override
-    public void setUp() throws Exception {
-        super.setUp();
-        getActivity();
-    }
+//    @Override
+//    public void setUp() throws Exception {
+//        super.setUp();
+//        before();
+//        getActivity();
+//    }
 
-    @Override
-    public void tearDown() throws Exception {
-    }
-
+    @Test
     public void test() {
 
         assertMainActivity();
@@ -59,7 +96,7 @@ public class ApplicationTesWithEspresso extends ActivityInstrumentationTestCase2
                 .check(matches(
                         AllOf.allOf(
                                 isDisplayed(),
-                                withText("Hello World")
+                                withText("Hello World!")
                         )));
 
         onView(withId(R.id.userId))
